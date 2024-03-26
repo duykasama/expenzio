85072df5b47d02302f3d70758f7d5160f2c96cf8
duykasama <duykasama@gmail.com>
2024-03-26 20:05:01 +0700

    feat: add breadcrumb

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,44 @@
+import Avatar from "./avatar";
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbSeparator,
+  } from "@/components/ui/breadcrumb"
+  
+
 type Props = {
     position: string;
 }
 
+const Something = () => {
+    return (
+        <Breadcrumb>
+            <BreadcrumbList>
+                <BreadcrumbItem>
+                <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                <BreadcrumbLink href="/components">Feature</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                <BreadcrumbLink href="/page">Page</BreadcrumbLink>
+                </BreadcrumbItem>
+            </BreadcrumbList>
+        </Breadcrumb>
+    )
+}
+
 const Header = ({position}: Props) => {
     return (
-        <header className={position}>This is header</header>
+        <header className={`${position} flex justify-between items-center p-8`}>
+
+            <Something />
+            <Avatar />
+        </header>
     );
 }
 
