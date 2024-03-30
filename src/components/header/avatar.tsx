b16dd9bdda4400d771d6f79dc1059fce72828b2c
duykasama <duykasama@gmail.com>
2024-03-30 10:54:38 +0700

    feat: add avatar

--- src/components/header/avatar.tsx
+++ src/components/header/avatar.tsx
@@ -1,6 +1,10 @@
+import PlaceholderAvatar from '@/assets/placeholder-avatar.png';
+
 const Avatar = () => {
     return (
-        <div>Avatar</div>
+        <div className='p-0.5 bg-[#2B32B2] rounded-full cursor-pointer'>
+            <img src={PlaceholderAvatar} alt="avatar" className='w-12' />
+        </div>
     );
 }
 
