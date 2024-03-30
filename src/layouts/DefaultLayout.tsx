6b7953462b45f660f1877b35495e6930b9d37cc6
duykasama <duykasama@gmail.com>
2024-03-30 10:59:36 +0700

    fix: make layout's main section expand to full width and height

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -8,7 +8,9 @@ const DefaultLayout = () => {
         <div className="bg-primary text-primary-foreground h-screen grid grid-cols-12 grid-rows-12 span">
             <NavigationBar position="col-span-2 row-span-11"/>
             <Header position="col-span-10 row-span-1"/>
-            <Outlet />
+            <main className="col-span-10 row-span-10">
+                <Outlet />
+            </main>
             <Footer position="col-span-12 row-span-1"/>
         </div>
     );
