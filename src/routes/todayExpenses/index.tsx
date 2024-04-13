e3072fab9fda4ebe3235b4788940e7f7c3c11c81
duykasama <duykasama@gmail.com>
2024-04-13 23:31:56 +0700

    feat: add dialog

--- src/routes/todayExpenses/index.tsx
+++ src/routes/todayExpenses/index.tsx
@@ -17,6 +17,15 @@ import {
     PaginationNext,
     PaginationPrevious,
   } from '@/components/ui/pagination';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+
 // import expenses from '@/data/expenses';
 import { FaEllipsisH } from 'react-icons/fa';
 import { gql, useQuery } from '@apollo/client';
@@ -38,57 +47,72 @@ const TodayExpenses = () => {
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error.message}</p>;
     return (
-        <section className="w-full h-full p-8 overflow-y-scroll">
-            <h1 className="text-3xl">Today expenses</h1>
-            <div className="flex justify-end my-4">
-                <Button className="w-fit border border-primary-foreground">Add new expense</Button>
-            </div>
-            <div className="flex flex-col justify-between">
-                <Table className="border">
-                    <TableCaption>A list of your recent expenses.</TableCaption>
-                    <TableHeader>
-                        <TableRow className="text-lg hover:bg-transparent">
-                            <TableHead className="w-[100px]">No.</TableHead>
-                            <TableHead>Category</TableHead>
-                            <TableHead>Price</TableHead>
-                            <TableHead>Created at</TableHead>
-                            <TableHead className="text-right">Action</TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {
-                            data.expenses.map((expense: any, idx: number) => ( // eslint-disable-line
-                                <TableRow key={idx}>
-                                    <TableCell>{idx+1}</TableCell>
-                                    <TableCell>{expense.category.name}</TableCell>
-                                    <TableCell>{expense.amount} VND</TableCell>
-                                    <TableCell>{expense.createdAt}</TableCell>
-                                    <TableCell className="flex justify-end">
-                                        <FaEllipsisH />
-                                    </TableCell>
-                                </TableRow>
-                            ))
-                        }
-                    </TableBody>
-                </Table>
-                <Pagination className="justify-end">
-                    <PaginationContent>
-                        <PaginationItem>
-                            <PaginationPrevious href="#" />
-                        </PaginationItem>
-                        <PaginationItem>
-                            <PaginationLink href="#">1</PaginationLink>
-                        </PaginationItem>
-                        <PaginationItem>
-                            <PaginationEllipsis />
-                        </PaginationItem>
-                        <PaginationItem>
-                            <PaginationNext href="#" />
-                        </PaginationItem>
-                    </PaginationContent>
-                </Pagination>
-            </div>
-        </section>
+        <>
+            <section className="w-full h-full p-8 overflow-y-scroll">
+                <h1 className="text-3xl">Today expenses</h1>
+                <div className="flex justify-end my-4">
+                    <Dialog>
+                        <DialogTrigger>
+                            <Button className="w-fit border border-primary-foreground">Add new expense</Button>
+                        </DialogTrigger>
+                        <DialogContent>
+                            <DialogHeader>
+                                <DialogTitle>Are you absolutely sure?</DialogTitle>
+                                <DialogDescription>
+                                This action cannot be undone. This will permanently delete your account
+                                and remove your data from our servers.
+                                </DialogDescription>
+                            </DialogHeader>
+                        </DialogContent>
+                    </Dialog>
+                </div>
+                <div className="flex flex-col justify-between">
+                    <Table className="border">
+                        <TableCaption>A list of your recent expenses.</TableCaption>
+                        <TableHeader>
+                            <TableRow className="text-lg hover:bg-transparent">
+                                <TableHead className="w-[100px]">No.</TableHead>
+                                <TableHead>Category</TableHead>
+                                <TableHead>Price</TableHead>
+                                <TableHead>Created at</TableHead>
+                                <TableHead className="text-right">Action</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {
+                                data.expenses.map((expense: any, idx: number) => ( // eslint-disable-line
+                                    <TableRow key={idx}>
+                                        <TableCell>{idx+1}</TableCell>
+                                        <TableCell>{expense.category.name}</TableCell>
+                                        <TableCell>{expense.amount} VND</TableCell>
+                                        <TableCell>{expense.createdAt}</TableCell>
+                                        <TableCell className="flex justify-end">
+                                            <FaEllipsisH />
+                                        </TableCell>
+                                    </TableRow>
+                                ))
+                            }
+                        </TableBody>
+                    </Table>
+                    <Pagination className="justify-end">
+                        <PaginationContent>
+                            <PaginationItem>
+                                <PaginationPrevious href="#" />
+                            </PaginationItem>
+                            <PaginationItem>
+                                <PaginationLink href="#">1</PaginationLink>
+                            </PaginationItem>
+                            <PaginationItem>
+                                <PaginationEllipsis />
+                            </PaginationItem>
+                            <PaginationItem>
+                                <PaginationNext href="#" />
+                            </PaginationItem>
+                        </PaginationContent>
+                    </Pagination>
+                </div>
+            </section>
+        </>
     );
 };
 
