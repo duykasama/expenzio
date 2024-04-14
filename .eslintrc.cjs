b0759c0176a7f60b3aa076a72345b87a61c0aa67
duykasama <duykasama@gmail.com>
2024-04-14 19:21:10 +0700

    feat: add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,30 @@
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'off',
-      { allowConstantExport: true },
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
     ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    "semi": [
-      'error',
-      'always'
-    ]
-  },
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': [
+            'off',
+            { allowConstantExport: true },
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        "semi": [
+            'error',
+            'always'
+        ]
+    },
+    eslintPluginPrettierRecommended,
 }
